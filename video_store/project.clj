a50b2d615976090790215ef6b752d229cbe23aa5
Harsh <shahmharsh@gmail.com>
2014-11-20 15:03:17 -0800

    Implemented get price, quantity and change movie price

--- video_store/project.clj
+++ video_store/project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [seesaw "1.4.4"]
-                 [me.raynes/fs "1.4.4"]]
+                 [me.raynes/fs "1.4.4"]
+                 [clj-time "0.8.0"]]
   :main ^:skip-aot video-store.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- video_store/src/video_store/inventory.clj
+++ video_store/src/video_store/inventory.clj
@@ -2,19 +2,24 @@
   (:gen-class)
   (:require [me.raynes.fs :as fs]))
 
-; inventory is a list, where each element in the list is a map.
+; inventory is a vector, where each element in the vector is a map.
 ; The map contains movie information and is of the format {:id ID, :name movie-name, :rental-price price, :quantity quantity}
 
+; renters is a list of renters, where each element in the vector is a map.
+; The map contains renters information and is of the format {:movie-name :renter-name :due-date}
+
 (def inventory [])
+(def renters [])
 
 (declare get-inventory)
 (declare exists-movie?)
 (declare position-in-inventory)
 (declare add-new-copies)
 (declare delete-movie)
+(declare find-movie-name)
 
 (defn- get-next-id
-  "Returns the next sequential ID"
+  "Returns the next sequential movie id."
   []
   (if (empty? inventory)
     1
@@ -39,19 +44,24 @@
   []
   (sort-by :id inventory))
 
+(comment (defn- key-not-equal-to
+  [key movie-name movie]
+  (not= movie-name (key movie))))
+
 (defn- exists-movie?
   "Returns true if movie exists in inventory."
   [movie-name]
   (not (empty? (drop-while #(not= movie-name (:name %)) inventory))))
 
 (defn add-new-copies
-  "Adds copies of movie to inventory. Returns nil. "
+  "Adds copies of movie to inventory. Throws 'MovieNotFoundException' if movie does
+  not exists. Returns nil."
   [movie-name copies]
   (if (exists-movie? movie-name)
     (let [index (position-in-inventory movie-name)]
       (def inventory (update-in inventory [index :quantity] #(+ copies %)))
       nil)
-    (throw (Exception. "MovieNotFoundInInventoryException"))))
+    (throw (Exception. "MovieNotFoundException"))))
 
 (defn- position-in-inventory
   "Returns the index of movie in inventory."
@@ -71,5 +81,63 @@
       (def inventory (into [] (concat (subvec inventory start index) (subvec inventory (+ index 1) end))))))
   nil)
 
+(defn change-movie-rental-price
+  "Updates movie with new price. Throws 'MovieNotFoundException' if movie does
+  not exists. Returns nil."
+  [movie-name price]
+  (if (exists-movie? movie-name)
+    (let [index (position-in-inventory movie-name)]
+      (def inventory (assoc-in inventory [index :rental-price] price))
+      nil)
+    (throw (Exception. "MovieNotFoundException"))))
 
+(defn- get-value-with-name
+  [movie-name key]
+  (if (exists-movie? movie-name)
+    (let [index (position-in-inventory movie-name)]
+      (key (get inventory index)))
+    (throw (Exception. "MovieNotFoundException"))))
+
+(defn find-movie-id
+  "Returns movie id. Throws 'MovieNotFoundException' if movie does not exists."
+  [movie-name]
+  (get-value-with-name movie-name :id))
+
+
+(defn find-price-with-name
+  "Returns rental-price of movie with movie-name. Throws 'MovieNotFoundException'
+  if movie does not exists."
+  [movie-name]
+  (get-value-with-name movie-name :rental-price))
+
+(defn find-quantity-with-name
+  "Returns quantity of movie with movie-name. Throws 'MovieNotFoundException'
+  if movie does not exists."
+  [movie-name]
+  (get-value-with-name movie-name :quantity))
+
+(defn find-price-with-id
+  "Returns rental-price of movie with id. Throws 'MovieNotFoundException'
+  if movie does not exists."
+  [id]
+  (get-value-with-name (find-movie-name id) :rental-price))
+
+(defn find-quantity-with-id
+  "Returns quantity of movie with id. Throws 'MovieNotFoundException'
+  if movie does not exists."
+  [id]
+  (get-value-with-name (find-movie-name id) :quantity))
+
+(defn find-movie-name
+  "Returns movie-name for id. Throws 'MovieNotFoundException' if movie does not exists"
+  [id]
+  (let [movie (first (drop-while #(not= id (:id %)) inventory))]
+    (if-not (nil? movie)
+      (:name movie)
+      (throw (Exception. "MovieNotFoundException")))))
+
+(defn rent-movie
+  ""
+  [movie-name renter-name]
+  )
 
